9ca467622c5b59a2663bcd97d690b711183103ff
John Hamlin <johnhamlin@gmail.com>
2023-12-28 16:05:47 -0500

    Add MDX support and update intro component

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -3,22 +3,46 @@
 import Image from 'next/image';
 import headshot from '@/public/John Hamlin Headshot.jpg';
 import { motion } from 'framer-motion';
+import Markdown from 'markdown-to-jsx';
+import IntroContent from '@/lib/intro.mdx';
 
 function Intro() {
   return (
     <section>
       <div className="flex items-center justify-center">
         <div className="relative">
-          <motion.div>
+          <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: 'tween',
+              duration: 0.2,
+            }}
+          >
             <Image
               src={headshot}
               alt="Headshot of John Hamlin"
               priority={true}
               className="h-48 w-48 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
             />
-            <span className="absolute text-7xl bottom-0 left-0">👋🏻</span>
+            <motion.span
+              className="absolute text-7xl bottom-0 left-0"
+              initial={{ opacity: 0, scale: 0 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{
+                type: 'spring',
+                stiffness: 125,
+                delay: 0.1,
+                duration: 0.7,
+              }}
+            >
+              👋🏻
+            </motion.span>
           </motion.div>
         </div>
+      </div>
+      <div className="prose prose-xl">
+        <IntroContent />
       </div>
     </section>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,11 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './lib/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
@@ -15,6 +16,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
-}
-export default config
+  plugins: [require('@tailwindcss/typography')],
+};
+export default config;
