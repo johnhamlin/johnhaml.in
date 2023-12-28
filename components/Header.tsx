ba1d9fcd7a7ff1204f3909b5620f8b7e73b67302
John Hamlin <johnhamlin@gmail.com>
2023-12-28 16:56:30 -0500

    Refactor component styles and remove unused dependencies

--- components/Header.tsx
+++ components/Header.tsx
@@ -20,13 +20,13 @@ function Header() {
         <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
           {links.map(link => (
             <motion.li
-              className="h-3/4 flex items-center justify-center relative"
+              className="relative flex items-center justify-center h-3/4"
               key={link.hash}
               initial={{ y: -100, opacity: 0 }}
               animate={{ y: 0, opacity: 1 }}
             >
               <Link
-                className="flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition"
+                className="flex items-center justify-center w-full px-3 py-3 transition hover:text-gray-950"
                 href={link.hash}
               >
                 {link.name}
--- components/Intro.tsx
+++ components/Intro.tsx
@@ -3,13 +3,12 @@
 import Image from 'next/image';
 import headshot from '@/public/John Hamlin Headshot.jpg';
 import { motion } from 'framer-motion';
-import Markdown from 'markdown-to-jsx';
 import IntroContent from '@/lib/intro.mdx';
 
 function Intro() {
   return (
     <section>
-      <div className="flex items-center justify-center">
+      <div className="flex flex-col items-center justify-center">
         <div className="relative">
           <motion.div
             initial={{ opacity: 0, scale: 0 }}
@@ -26,7 +25,7 @@ function Intro() {
               className="h-48 w-48 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
             />
             <motion.span
-              className="absolute text-7xl bottom-0 left-0"
+              className="absolute bottom-0 left-0 text-7xl"
               initial={{ opacity: 0, scale: 0 }}
               animate={{ opacity: 1, scale: 1 }}
               transition={{
@@ -40,9 +39,10 @@ function Intro() {
             </motion.span>
           </motion.div>
         </div>
-      </div>
-      <div className="prose prose-xl">
-        <IntroContent />
+        {/* Leading changes the line spacing. Need the ! to overide the line spacing in prose-2xl */}
+        <div className="px-4 mt-4 mb-10 font-medium prose prose-2xl prose-p:!leading-[1.5] sm:prose-p:text-4xl">
+          <IntroContent />
+        </div>
       </div>
     </section>
   );
