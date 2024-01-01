0e4d1d74e7eae733e61d73f5cff4aff65d559ac1
John Hamlin <johnhamlin@gmail.com>
2024-01-01 16:39:11 -0500

    Update Project component and update .gitignore

--- components/Project.tsx
+++ components/Project.tsx
@@ -31,9 +31,9 @@ function Project({
       }}
     >
       {/* Card container */}
-      <div className=" relative max-w-[42rem] overflow-hidden rounded-xl border border-black/5 bg-gray-100  transition  hover:bg-gray-200 sm:h-[20rem] sm:pr-8">
+      <div className=" relative max-w-[42rem] overflow-hidden rounded-xl border border-black/5 bg-gray-100  transition  hover:bg-gray-200 sm:h-[20rem] sm:pr-8 sm:group-even:pl-8">
         {/* Text Container on the left */}
-        <div className="flex h-full flex-col px-5 pb-7 pt-4 group-odd:mr-[20rem] group-even:ml-[20rem] sm:top-10 sm:max-w-[50%] sm:pl-10 sm:pr-2">
+        <div className="flex h-full flex-col px-5 pb-7 pt-4 sm:max-w-[50%] sm:pl-10 sm:pr-2 sm:pt-10 sm:group-even:ml-[18rem]">
           <h3 className="text-2xl font-semibold">{title}</h3>
           <p className="mt-2 leading-relaxed text-gray-700">{description}</p>
           <ul className="flex flex-wrap gap-2 mt-4 sm:mt-auto">
@@ -53,14 +53,18 @@ function Project({
           src={imageUrl}
           alt={title}
           quality={95}
-          className="group-even:-right-[initial] absolute -right-40 top-8 w-[28.05rem] rounded-t-lg
-        shadow-2xl transition group-even:-left-40
-        group-even:right-[initial] group-hover:-translate-x-3 group-hover:translate-y-3 group-hover:-rotate-2 
-        
-        
-        group-hover:scale-[1.04] group-even:group-hover:translate-x-3 group-even:group-hover:translate-y-3
-        
-        group-even:group-hover:rotate-2 "
+          className="absolute -right-40 top-8 hidden w-[28.25rem] rounded-t-lg shadow-2xl transition
+          group-even:-left-40 
+          group-even:right-[initial]
+          group-hover:-translate-x-3
+          group-hover:translate-y-3
+          group-hover:-rotate-2
+  
+          group-hover:scale-[1.04]
+          group-even:group-hover:translate-x-3
+          group-even:group-hover:translate-y-3
+  
+          group-even:group-hover:rotate-2 sm:block"
         />
       </div>
     </motion.div>
