3d896a4b8582f53a4d770d8d3e272fbf935750b9
John Hamlin <johnhamlin@gmail.com>
2024-01-12 12:17:04 -0500

    Commented out unused import and updated project description

--- content/experience.ts
+++ content/experience.ts
@@ -1,5 +1,5 @@
 import React from 'react';
-import { CgWorkAlt } from 'react-icons/cg';
+// import { CgWorkAlt } from 'react-icons/cg';
 import { FaReact } from 'react-icons/fa';
 import { LuGraduationCap } from 'react-icons/lu';
 import { SiApachekafka } from 'react-icons/si';
