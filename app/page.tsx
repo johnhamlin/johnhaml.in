e8f9588d2847586f9bfcccfabc24b00ed71d0dbc
John Hamlin <johnhamlin@gmail.com>
2024-02-12 13:17:34 -0500

    Add keys to meta tags

--- app/page.tsx
+++ app/page.tsx
@@ -13,29 +13,53 @@ export default function Home() {
       {/* Doing this manually because generating it with Next.js is not working */}
       <Head>
         {/* Facebook */}
-        <meta property="og:url" content="https://johnhaml.in" />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content="John Hamlin's Portfolio" />
+        <meta property="og:url" content="https://johnhaml.in" key={'og:url'} />
+        <meta property="og:type" content="website" key={'og:type'} />
+        <meta
+          property="og:title"
+          content="John Hamlin's Portfolio"
+          key={'og:title'}
+        />
         <meta
           property="og:description"
           content="John Hamlin is a full-stack engineer in Raleigh, N.C."
+          key={'og:description'}
         />
         <meta
           property="og:image"
           content="https://johnhaml.in/opengraph-image.jpg"
+          key={'og:image'}
         />
         {/* Twitter */}
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta property="twitter:domain" content="johnhaml.in" />
-        <meta property="twitter:url" content="https://johnhaml.in" />
-        <meta name="twitter:title" content="John Hamlin's Portfolio" />
+        <meta
+          name="twitter:card"
+          content="summary_large_image"
+          key={'twitter:card'}
+        />
+        <meta
+          property="twitter:domain"
+          content="johnhaml.in"
+          key={'twitter:domain'}
+        />
+        <meta
+          property="twitter:url"
+          content="https://johnhaml.in"
+          key={'twitter:url'}
+        />
+        <meta
+          name="twitter:title"
+          content="John Hamlin's Portfolio"
+          key={'twitter:title'}
+        />
         <meta
           name="twitter:description"
           content="John Hamlin is a full-stack engineer in Raleigh, N.C."
+          key={'twitter:description'}
         />
         <meta
           name="twitter:image"
           content="https://johnhaml.in/opengraph-image.jpg"
+          key={'twitter:image'}
         />
       </Head>
       <main className="flex flex-col items-center px-4 ">
