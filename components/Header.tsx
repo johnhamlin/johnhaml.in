729fd190e19d169b9557208d8b2edbf4f51b5256
John Hamlin <johnhamlin@gmail.com>
2025-03-27 16:12:24 -0400

    Fix styling on desktop - Cursor made changes not using tailwind and not respecting desktop

--- components/Header.tsx
+++ components/Header.tsx
@@ -15,18 +15,16 @@ function Header() {
     <header className="relative z-[999]">
       {/* white bg */}
       <motion.div
-        className="fixed left-1/2 top-0 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] dark:border-black/40 dark:bg-gray-950 dark:bg-opacity-75 sm:top-6 sm:h-[3.25rem] sm:w-[31rem] sm:rounded-full"
+        className="pt-[env(safe-area-inset-top)] h-[calc(4.5rem + env(safe-area-inset-top))] fixed left-1/2 top-0 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] dark:border-black/40 dark:bg-gray-950 dark:bg-opacity-75 sm:top-6 sm:h-[3.25rem] sm:w-[31rem] sm:rounded-full"
         initial={{ y: -200, x: '-50%', opacity: 0 }}
         animate={{ y: 0, x: '-50%', opacity: 1 }}
-        style={{
-          paddingTop: 'env(safe-area-inset-top)',
-          height: 'calc(4.5rem + env(safe-area-inset-top))',
-        }}
       ></motion.div>
 
       {/* Links */}
-      <nav className="fixed left-1/2 top-[0.15rem] flex h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0" style={{ paddingTop: 'calc(0.15rem + env(safe-area-inset-top))' }}>
-        <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
+      <nav
+        className="pt-[calc(0.15rem + env(safe-area-inset-top))] fixed left-1/2 top-[0.15rem] flex h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0"
+      >
+        <ul className="flex flex-wrap items-center justify-center gap-y-1 font-medium text-gray-500 w-[22rem] text-[0.9rem] sm:w-[initial] sm:flex-nowrap sm:gap-5">
           {links.map((link) => (
             <motion.li
               className="relative flex h-3/4 items-center justify-center"
